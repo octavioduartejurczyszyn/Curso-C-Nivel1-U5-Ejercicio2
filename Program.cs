b6dd11e0b2ab5ee90752fd487d816e6cbd8ef97c
octavioduartejurczyszyn <tatoduarte9@gmail.com>
2024-02-20 13:31:36 +0100

    Primera subida

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ejercico2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. Hacer un programa que solicite el ingreso de 10 números y 
+            //que muestre el mayor de ellos por pantalla. Solo se debe emitir UN valor por pantalla.
+            int  a, mayor = 0;
+            for (int x = 0; x < 10; x++)
+            {
+                Console.WriteLine("Escriba un nro: ");
+                a = int.Parse(Console.ReadLine());
+                    if (x == 0)
+                    mayor = a;
+                    else if(a > mayor)
+                    mayor = a;
+            }
+            Console.WriteLine("El mayor es: " + mayor);
+
+                    
+
+
+        }
+    }
+}
